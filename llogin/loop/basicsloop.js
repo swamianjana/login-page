200338849ae04471057f04fb01d3be70e4daa51a
Anjana <anjana20swami@gmail.com>
2022-08-14 19:13:52 +0530

    learn login page

---
+++ llogin/loop/basicsloop.js
@@ -0,0 +1,17 @@
+console.log("welcome to universe");
+// console.log(1)
+// console.log(2)
+// console.log(3)
+
+//  generally Loops are 3 types : for , while , do-while 
+
+// let a =34;
+// a += 1;
+// a++;
+// console.log(a)
+for(let i=0; i<100;i++)
+{
+    console.log(i);
+}
+
+let j = 0;
